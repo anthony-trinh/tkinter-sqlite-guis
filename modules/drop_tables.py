b3cc4c0ab3f0ec1e2960f9ce6549851f1c10dfa6
Anthony Trinh <47074388+anthony-trinh@users.noreply.github.com>
2020-12-02 01:55:57 -0500

    Committed new file
    Finished development of a function for dropping all the tables in database

---
+++ modules/drop_tables.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+conn = sqlite3.connect('hospital.db') 
+
+c = conn.cursor()
+
+c.execute("""
+    DROP TABLE hospitals ;
+    """)
+
+c.execute("""
+    DROP TABLE employees ;
+    """)
+
+c.execute("""
+    DROP TABLE doctors ;
+    """)
+
+c.execute("""
+    DROP TABLE nurses ;
+    """)
+
+c.execute("""
+    DROP TABLE patients ;
+    """)
+
+c.execute("""
+    DROP TABLE appointments ;
+    """)
+
+c.execute("""
+    DROP TABLE invoices ;
+    """)
+
+c.execute("""
+    DROP TABLE medicines
+    """)
+
+c.execute("""
+    DROP TABLE diagnosises
+    """)
+
+c.execute("""
+    DROP TABLE prescriptions
+    """)
+
+c.execute("""
+    DROP TABLE medical_history
+    """)
+
+conn.commit()
+
+conn.close()
